cd7813358fb17fc4f395c21275987a9134f47bb4
Uday Manish Barapatre <uday12patre.code@gmail.com>
2026-08-30 13:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.53%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+
+     long BinForm = n;
+     if(n < 0)
+     {
+        x = 1/x;
+        BinForm = -BinForm; 
+     }
+
+     double ans = 1;
+
+     while(BinForm > 0)
+     {
+        if(BinForm % 2 == 1)
+            ans *= x;
+        x *= x;
+        BinForm /= 2;
+     }
+     
+     return ans;
     }
 };
