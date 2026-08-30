48b8867ee9671e35a87f6a97ba0b8f2c6869c72c
Uday Manish Barapatre <uday12patre.code@gmail.com>
2026-08-30 11:42:08 +0530

    Time: 8 ms (41.48%) | Memory: 13.5 MB (17.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
+        unordered_map<char, int> roman = 
+        {
             {'I', 1},
             {'V', 5},
             {'X', 10},
@@ -10,19 +11,18 @@ public:
             {'D', 500},
             {'M', 1000}
         };
-        
+
         int total = 0;
         int n = s.length();
-        
-        for (int i = 0; i < n; ++i) {
-            // If current symbol has smaller value than the next symbol, subtract it
-            if (i + 1 < n && roman[s[i]] < roman[s[i + 1]]) {
+
+        for(int i = 0; i < n; i++)
+        {
+            if(i + 1 < n && roman[s[i]] < roman[s[i+1]])
                 total -= roman[s[i]];
-            } else {
+            else
                 total += roman[s[i]];
-            }
         }
-        
+
         return total;
     }
 };
